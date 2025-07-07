bef9c6d3d1673c22a227fed39d246b5a48d79542
Fl4gell4nt <rene.schweidler@gmail.com>
2025-07-07 14:01:06 +0200

    fixed unit test so it will ignore the templates and the data ontologies

--- tests/test_parse_ttl.py
+++ tests/test_parse_ttl.py
@@ -2,8 +2,11 @@ import glob
 from rdflib import Graph
 import pytest
 
-# Collect all Turtle files under rdf/ and data/
-ttl_files = glob.glob('rdf/**/*.ttl', recursive=True) + glob.glob('data/**/*.ttl', recursive=True)
+# Collect all Turtle files under rdf/ and data/, skipping templates and test data
+ttl_files = [
+    f for f in (glob.glob('rdf/**/*.ttl', recursive=True) + glob.glob('data/**/*.ttl', recursive=True))
+    if 'template' not in f and not f.startswith('rdf/__')
+]
 
 @pytest.mark.parametrize('ttl_file', ttl_files)
 def test_parse_ttl_files(ttl_file):
